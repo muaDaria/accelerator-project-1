fa58f87548eda3cf8700c0681848ae700172f42f
Daria <dashusik_@mail.ru>
2025-03-16 20:11:34 +0300

    ADD: video.js

---
+++ source/js/video.js
@@ -0,0 +1,26 @@
+import { isEnterKey } from './util.js';
+
+const playButton = document.getElementById('playButton');
+const videoContainer = document.querySelector('.about__video');
+
+const videoUrl = 'https://www.youtube.com/embed/watch?v=9TZXsZItgdw?autoplay=1';
+
+const uploadVideo = () => {
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('src', videoUrl);
+  iframe.setAttribute('loading', 'lazy');
+  iframe.setAttribute('allowfullscreen', 'true');
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+
+  videoContainer.innerHTML = '';
+  videoContainer.appendChild(iframe);
+};
+
+playButton.addEventListener('click', uploadVideo);
+
+playButton.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    uploadVideo();
+  }
+});
