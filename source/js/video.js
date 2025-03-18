e4c3cd33cbbf88b8ac09a8ce2bcce0a4486ec4f2
Daria <dashusik_@mail.ru>
2025-03-18 19:23:15 +0300

    FIX: about pp

--- source/js/video.js
+++ source/js/video.js
@@ -3,15 +3,18 @@ import { isEnterKey } from './util.js';
 const playButton = document.querySelector('.about__play-button');
 const videoContainer = document.querySelector('.about__video');
 
-const videoUrl = 'https://www.youtube.com/embed/watch?v=9TZXsZItgdw?autoplay=1';
+const videoUrl = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
 
 const uploadVideo = () => {
   const iframe = document.createElement('iframe');
+  iframe.classList.add('about__iframe');
   iframe.setAttribute('src', videoUrl);
   iframe.setAttribute('loading', 'lazy');
   iframe.setAttribute('allowfullscreen', 'true');
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
+  iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('referrerpolicy', 'trict-origin-when-cross-origin');
+  iframe.setAttribute('title', 'YouTube video player');
 
   videoContainer.innerHTML = '';
   videoContainer.appendChild(iframe);
