00c70f3cab1d55346a84a9f67509848dba575fcd
Daria <dashusik_@mail.ru>
2025-03-23 21:17:07 +0300

    ADD: slider

---
+++ source/js/slider.js
@@ -0,0 +1,43 @@
+import Swiper from './vendor/swiper.mjs';
+import Mousewheel from './vendor/modules/mousewheel.min.mjs';
+import Navigation from './vendor/modules/navigation.min.mjs';
+
+new Swiper('.swiper', {
+  modules: [Navigation, Mousewheel],
+
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: 'pointer',
+  watchOverflow: true,
+  centeredSides: false,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  Mousewheel: {
+    forceToAxis: true,
+  },
+  //scrollbar: {
+  // el: '.swiper-scrollbar',
+  //},
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      initialSlide: 0,
+      simulateTouch: true,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      simulateTouch: false,
+    },
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      simulateTouch: false,
+    }
+  }
+});
